efdc768c105232098da04bbf7b4408fb0d93e472
peteryao <peteryao.gm@gmail.com>
2025-01-13 17:28:13 +0800

    upgrade the tests folder to 14.1

--- tests/smoke_test/test_basic.py
+++ tests/smoke_test/test_basic.py
@@ -36,6 +36,26 @@ def test_init(shell):
 
 
 @pytest.mark.dependency(depends=["test_init"])
+def test_rauc_status(shell, shell_json):
+    rauc_status = shell.run_check("rauc status --output-format=shell --detailed")
+    # RAUC_BOOT_PRIMARY won't be set if correct grub env is missing
+    assert "RAUC_BOOT_PRIMARY='kernel.0'" in rauc_status
+    assert "rauc-WARNING" not in "\n".join(rauc_status)
+
+    os_info = shell_json("ha os info --no-progress --raw-json")
+    expected_version = os_info.get("data", {}).get("version")
+    assert expected_version is not None and expected_version != ""
+
+    boot_slots = filter(lambda x: "RAUC_SYSTEM_SLOTS=" in x, rauc_status)
+    boot_slots = next(boot_slots, "").replace("RAUC_SYSTEM_SLOTS='", "").replace("'", "")
+    assert boot_slots != ""
+    booted_idx = boot_slots.split(" ").index("kernel.0")
+    assert booted_idx >= 0
+
+    assert f"RAUC_SLOT_STATUS_BUNDLE_VERSION_{booted_idx + 1}='{expected_version}'" in rauc_status
+
+
+@pytest.mark.dependency(depends=["test_init"])
 def test_dmesg(shell):
     output = shell.run_check("dmesg")
     _LOGGER.info("%s", "\n".join(output))
@@ -51,3 +71,8 @@ def test_supervisor_logs(shell):
 def test_systemctl_status(shell):
     output = shell.run_check("systemctl --no-pager -l status -a || true")
     _LOGGER.info("%s", "\n".join(output))
+
+@pytest.mark.dependency(depends=["test_init"])
+def test_systemctl_check_no_failed(shell):
+    output = shell.run_check("systemctl --no-pager -l list-units --state=failed")
+    assert "0 loaded units listed." in output, f"Some units failed:\n{"\n".join(output)}"
